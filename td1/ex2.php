d534e76f193fb4bc1d9dfac53387e700a7548147
YannisYN <21901041@etu.unicaen.fr>
2020-03-18 11:40:59 +0100

    Ajout du fichier de test

--- td1/ex2.php
+++ td1/ex2.php
@@ -1,6 +1,26 @@
-<a href="?message=Premier lien">1<a>
-<a href="?message=2ème lien">2<a>
-<a href="?message=3ème lien">3<a>
-<h1>
-    <?=$_GET['message']??'pas de message' ?>
-</h1>
+<?php
+/*$message=$_GET["message"]??"test de message";
+$size=$_GET["size"]??"10";
+$color=$_GET["color"]??"black";*/
+$size=5;$color='red';$message="no msg";
+extract($_GET);
+?>
+
+
+    <a href="?size=30">en 30<a>
+<a href="?color=red">en rouge<a>
+<a href="?message=salut&color=green">Message en vert<a>
+<a href="?size=60&color=blue">Message en bleu et 60<a>
+
+    
+<form method="GET" action="">
+    <textarea name="message"><?=$message?></textarea>
+    <input type="color" name="color" value="<?=$color?>">
+    <input type="number" name="size" value="<?=size?>">
+    <button>Valider</button>
+                             
+</form>
+
+<div style="font-size: <?=$size?>px;color:<?=$color?>"> <?=$message?> </div>
+<?php
+echo $size;
