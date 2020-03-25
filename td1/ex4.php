885e5af52b58e22724ac6d42ac99172bb02d5405
YannisYN <21901041@etu.unicaen.fr>
2020-03-25 11:13:13 +0100

    Exercice 4 terminé

--- td1/ex4.php
+++ td1/ex4.php
@@ -1,30 +1,30 @@
+<?php
+$title='Ex4';
+include "include/header.php";
+?>
 <a href="?l=2&c=2">Tableau 2 x 2</a><br>
 <a href="?l=5&c=5">Tableau 5 x 5</a><br>
 <a href="?l=10&c=8">Tableau 10 x 8</a><br>
 <hr>
 <?php
-$title='Exercice n°4 (Itérations)';
-include "include/header.php";
 //Récupère les paramètres l et c passés dans l'url
-$nbLignes=$_GET["l"]??10;
-$nbColonnes=$_GET["c"]??10;
-echo "<h2>Tableau de $nbLignes par $nbColonnes</h2>";
+$nblignes=$_GET["l"]??10;
+$nbcolonnes=$_GET["c"]??10;
+echo "<h2>Tableau de $nblignes par $nbcolonnes</h2>";
 ?>
-
 <table border='1'>
     <tbody>
-      <?php for($i=0;$i<$nbLignes;$i++){?>
+      <?php for($i=0;$i<$nblignes;$i++){
+        $gras=($i%2==0)?"bold":"normal";
+      ?>
         <tr>
-        <?php for($j=0;$j<$nbColonnes;$j++){?>
-            <td><?="$i - $j"?></td>
+        <?php for($j=0;$j<$nbcolonnes;$j++){
+            $rouge=($j%2==0)?"red":"black";
+            ?>
+            <td style="font-weight: <?=$gras ?>;color:<?=$rouge ?>"><?="$i - $j"?></td>
         <?php }?>
         </tr>
-      <?php
-       if() {}                                
-                                       
-                                       
-                                       
-                        }?>
+      <?php }?>
     </tbody>
 </table>
 <?php
