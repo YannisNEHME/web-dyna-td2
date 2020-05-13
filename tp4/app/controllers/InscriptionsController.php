aab26da80d905da31590a1646eccd970e63e4fd6
YannisYN <21901041@etu.unicaen.fr>
2020-05-13 10:34:12 +0200

    5eme commit TP4

--- tp4/app/controllers/InscriptionsController.php
+++ tp4/app/controllers/InscriptionsController.php
@@ -5,6 +5,8 @@ use Ubiquity\orm\DAO;
 use Ubiquity\utils\http\URequest;
 use Ubiquity\utils\http\UResponse;
 use Ubiquity\utils\http\USession;
+use models\Inscription;
+
  /**
  * Controller InscriptionsController
  **/
@@ -17,12 +19,25 @@ class InscriptionsController extends ControllerBase{
 
 public function Inscription(){
 
-    UResponse::header("location" ,"/");
- 
+    		$this->loadView("InscriptionsController/inscription.html");
+
+    }
     
-    USession::exists("coucou");
-    DAO::insert("coucou");
-    URequest::SetValuesToObject("coucou");
+    
+public function do(){
+    
+    $variable = new Inscription();
+        $variable->setNom($_POST["pseudo"]);
+        $variable->setEmail($_POST["email"]);
+        DAO::insert($variable);
+    
+    if(DAO::insert($var)){
+            echo "Confirmé, redirection...";
+            UResponse::header("location" ,"/inscriptionsController"); 
+        }
+        else{
+            echo "<div class='ui red message'><p>Votre adresse Mail " . $_POST["email"] . " n'a pas fonctionnée pour votre inscription</p><a href='/inscriptionsController'>Retour à la page d'accueil</a></div>";
+        }
     
     }
 }
