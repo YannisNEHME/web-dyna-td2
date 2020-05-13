fcd5418d6e7411ca673724c4c68dbdaa7a313c4f
YannisYN <21901041@etu.unicaen.fr>
2020-05-13 09:02:45 +0200

    2eme commit TP4

--- tp4/app/controllers/InscriptionsController.php
+++ tp4/app/controllers/InscriptionsController.php
@@ -8,4 +8,11 @@ class InscriptionsController extends ControllerBase{
 	public function index(){
 		$this->loadView("InscriptionsController/index.html");
 	}
+
+
+public function Inscription(){
+
+        $this->loadView('InscriptionsController/Inscription.html');
+
+    }
 }
